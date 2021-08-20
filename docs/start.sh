dc31e477bfd99321cb965bf95cf3a825d6143ff8
stormmurdoc <murdoc@storm-clan.de>
2021-08-20 21:01:11 +0200

    github workflow test5

--- docs/start.sh
+++ docs/start.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env sh
+#
+# -e: stops the script on errors
+# -u: stops the script on unset variables
+# -o pipefail : fail the whole pipeline if
+#               a command fail
+
+set -euo pipefail
 
 ./getdwmkeys.sh ../config.h markdown
 
